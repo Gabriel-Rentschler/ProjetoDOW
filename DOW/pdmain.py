f72163968646fb002aaec38c55577a94fbd51729
Gabriel Rentschler de Araujo <30481829+Gabriel-Rentschler@users.noreply.github.com>
2021-05-05 19:01:52 -0300

    Leitura de mais  de uma planilha

--- DOW/pdmain.py
+++ DOW/pdmain.py
@@ -4,16 +4,31 @@ import sys
 path = sys.argv[1]
 
 xl = pd.ExcelFile(path)
-dfs = pd.read_excel(path)
+df1 = pd.read_excel(path, sheet_name=0)
+df2 = pd.read_excel(path, sheet_name=1)
 
+numSheets = len(xl.sheet_names)
 print(xl.sheet_names)
+print(numSheets)
 
-for column in range(dfs.shape[1]):
-    for row in range(dfs.shape[0]):
-        cell = dfs.iat[row, column]
+for sheets in range(numSheets):
 
-        if pd.notnull(dfs.iat[row, column]):
-            print(dfs.iat[row, column])
+    res = input("Print sheet?")
+    if res == "n":
+        break
+    if res == "y":
+        df1 = pd.read_excel(path, sheet_name = sheets)
+        print("Nome da planilha: " + df1.sheet_name)
+
+
+    for row in range(df1.shape[0]):
+        for column in range(df1.shape[1]):
+            cell = df1.iat[row, column]
+
+            if pd.notnull(df1.iat[row, column]):
+                print(str(df1.iat[row, column]) + ' | ', end = '')
+
+        print("")
 
 #print(dfs.shape[0])
 #print(dfs.shape[1])
