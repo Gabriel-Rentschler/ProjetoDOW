bf2bc8d5db4c29b3b62e3d39e35a2725a219e247
Gabriel Rentschler de Araujo <30481829+Gabriel-Rentschler@users.noreply.github.com>
2021-05-06 17:25:52 -0300

    Criação de script para leitura de tabelas
    e printar nome de cada planilha

--- DOW/pdmain.py
+++ DOW/pdmain.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import sys
+import readSheet
 
 path = sys.argv[1]
 
 xl = pd.ExcelFile(path)
 df1 = pd.read_excel(path, sheet_name=0)
-df2 = pd.read_excel(path, sheet_name=1)
 
 numSheets = len(xl.sheet_names)
-print(xl.sheet_names)
-print(numSheets)
 
 for sheets in range(numSheets):
 
@@ -18,19 +16,14 @@ for sheets in range(numSheets):
         break
     if res == "y":
         df1 = pd.read_excel(path, sheet_name = sheets)
-        print("Nome da planilha: " + df1.sheet_name)
-
+        print('')
+        print('Nome da planilha: %s' %xl.sheet_names[sheets])
 
     for row in range(df1.shape[0]):
         for column in range(df1.shape[1]):
             cell = df1.iat[row, column]
 
             if pd.notnull(df1.iat[row, column]):
-                print(str(df1.iat[row, column]) + ' | ', end = '')
+                readSheet.lePlanilha(cell)
 
-        print("")
-
-#print(dfs.shape[0])
-#print(dfs.shape[1])
-
-#print(dfs.loc[3])
+        print('')
---
+++ DOW/readSheet.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def lePlanilha(cell):
+    print(str(cell) + ' | ', end = '')   
+
